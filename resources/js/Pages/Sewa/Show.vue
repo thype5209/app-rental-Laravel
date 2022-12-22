10c17aa77b622f655cbbd1353a0881c28b811240
Agustiawan <44895587+thype5209@users.noreply.github.com>
2022-12-22 10:25:42 +0800

    Update Show.vue Sewa

--- resources/js/Pages/Sewa/Show.vue
+++ resources/js/Pages/Sewa/Show.vue
@@ -15,123 +15,123 @@
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Kode Sewa</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         {{ sewa.kode }}</td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Harga Sewa</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         Rp. {{ sewa.harga }}</td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Unit Mobil</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         {{ sewa.unit }}/{{ sewa.nopol }}/{{
                                 sewa.tahun
                         }} </td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Penyewa</td>
-                    <td class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold"
+                    <td class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold"
                         v-if="sewa.pengguna != null">{{ sewa.pengguna.nama }}/{{ sewa.nik }}</td>
-                    <td class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold"
+                    <td class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold"
                         v-else>{{ sewa.nik }}</td>
                 </tr>
                 <tr v-if="sewa.pengguna != null">
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Alamat Penyewa</td>
-                    <td class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold"
+                    <td class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold"
                         v-if="sewa.pengguna != null">{{ sewa.pengguna.alamat }}</td>
                 </tr>
                 <tr v-if="sewa.pengguna != null">
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Nomor HP Penyewa</td>
-                    <td class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold"
+                    <td class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold"
                         v-if="sewa.pengguna != null">{{ sewa.pengguna.no_hp }}/{{ sewa.pengguna.no_hp_lain }} </td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Tujuan</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         {{ sewa.tujuan }}</td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Jaminan</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         {{ sewa.jaminan }}</td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Tanggal sewa</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         {{ sewa.waktusewa.tgl_sewa }}</td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Tanggal Kembali</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         {{ sewa.waktusewa.tgl_kembali }}</td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Lama Sewa</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         {{ sewa.waktusewa.lama_sewa }} Hari</td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Jumlah Telat Per Jam </td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         {{ sewa.waktusewa.telat }} JAM</td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Jumlah Bayar</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         Rp. {{ reduceArray(sewa.harga) }} </td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Denda</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         Rp. {{ reduceArray(sewa.denda) }} </td>
                 </tr>
                 <tr>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap">
                         Total Bayar</td>
                     <td
-                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
+                        class=" border border-gray-200 md:px-3 sm:py-2 p-1 text-default-dark text-sm sm:text-base capitalize whitespace-pre-wrap font-semibold">
                         Rp. {{ reduceArray(sewa.harga , sewa.waktusewa.lama_sewa, sewa.denda) }} </td>
                 </tr>
 
